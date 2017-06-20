0d44a368e127ab8a3f05bad40eb1fe6f0738cef2
chen.dong <chen_dong85@foxmail.com>
2017-06-20 22:59:16 +0800

    add the tree and insert and travers function

---
+++ js/tree/src/tree.js
@@ -0,0 +1,76 @@
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(key) {
+    const newNode = new Node(key);
+    if (this.root === null) {
+      this.root = newNode
+    } else {
+      this.insertNode(newNode, this.root)
+    }
+  }
+
+  insertNode(newNode, node) {
+    if (newNode.key > node.key) {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(newNode, node.right)
+      }
+    } else {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(newNode, node.left)
+      }
+    }
+  }
+
+  inOrderTraverse(callback) {
+    this.inOrderTraverseNode(this.root, callback)
+  }
+
+  inOrderTraverseNode(node, callback) {
+    if (node != null) {
+      this.inOrderTraverseNode(node.left, callback);
+      callback(node)
+      this.inOrderTraverseNode(node.right, callback);
+    }
+  }
+
+  preOrderTraverse(callback) {
+    this.preOrderTraverseNode(this.root, callback)
+  }
+
+  preOrderTraverseNode(node, callback) {
+    if (node !== null) {
+      callback(node)
+      this.preOrderTraverseNode(node.left, callback);
+      this.preOrderTraverseNode(node.right, callback);
+    }
+  }
+
+  postOrderTraverse(callback) {
+    this.postOrderTraverseNode(this.root, callback)
+  }
+
+  postOrderTraverseNode(node, callback) {
+    if (node !== null) {
+      this.preOrderTraverseNode(node.left, callback);
+      this.preOrderTraverseNode(node.right, callback);
+      callback(node)
+    }
+  }
+}
+
+module.exports = BinarySearchTree;
---
+++ js/tree/test/tree-test.js
@@ -0,0 +1,56 @@
+const chai = require('chai');
+const BinarySearchTree = require('../src/tree');
+
+const expect = chai.expect;
+
+describe('BSTree', () => {
+
+  it('should insert collect node into the tree', () => {
+    const bstree = new BinarySearchTree();
+    bstree.insert(11);
+    bstree.insert(12);
+    bstree.insert(10);
+
+    expect(bstree.root.key).to.equal(11)
+    expect(bstree.root.left.key).to.equal(10)
+    expect(bstree.root.right.key).to.equal(12);
+  })
+
+  it('should return the inOrder Traverse tree node ', () => {
+    const result = [];
+    const callback = node => result.push(node.key);
+
+    const bstree = new BinarySearchTree();
+    bstree.insert(11);
+    bstree.insert(12);
+    bstree.insert(10);
+    bstree.inOrderTraverse(callback)
+    expect(result).to.deep.equal([10, 11, 12])
+
+  })
+
+  it('should return the preOrder Traverse tree node ', () => {
+    const result = [];
+    const callback = node => result.push(node.key);
+
+    const bstree = new BinarySearchTree();
+    bstree.insert(11);
+    bstree.insert(12);
+    bstree.insert(10);
+    bstree.preOrderTraverse(callback)
+    expect(result).to.deep.equal([11, 10, 12])
+  })
+
+  it('should return the post Order Traverse tree node ', () => {
+    const result = [];
+    const callback = node => result.push(node.key);
+
+    const bstree = new BinarySearchTree();
+    bstree.insert(11);
+    bstree.insert(12);
+    bstree.insert(10);
+    bstree.postOrderTraverse(callback)
+    expect(result).to.deep.equal([10, 12, 11])
+  })
+
+})
