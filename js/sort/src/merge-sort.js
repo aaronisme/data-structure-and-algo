2fccef7da34d61b138091d9d3b066dcbc332f652
chen.dong <chen_dong85@foxmail.com>
2017-07-04 22:52:18 +0800

    add the merge sort

--- js/sort/src/merge-sort.js
+++ js/sort/src/merge-sort.js
@@ -1,3 +1,45 @@
-const mergeSort = (items) =>{
+const mergeSortArray = (items) => {
+  return mergeSort(items);
+}
 
+const mergeSort = (items) => {
+  const length = items.length;
+  if (length == 1) {
+    return items
+  }
+
+  const mid = Math.floor(length / 2)
+  const left = items.slice(0, mid);
+  const right = items.slice(mid, length);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+
+const merge = (leftArray, rightArray) => {
+  const result = [];
+  let i = 0;
+  let j = 0;
+  while (i < leftArray.length && j < rightArray.length) {
+    if (leftArray[i] < rightArray[j]) {
+      result.push(leftArray[i++])
+    } else {
+      result.push(rightArray[j++])
+    }
+  }
+
+  while (i < leftArray.length) {
+    result.push(leftArray[i++])
+  }
+
+  while (j < rightArray.length) {
+    result.push(rightArray[j++])
+  }
+
+  return result;
+}
+
+
+module.exports = {
+  mergeSortArray
 }
--- js/sort/test/insert-sort-test.js
+++ js/sort/test/insert-sort-test.js
@@ -2,7 +2,7 @@ const sorts = require('../src/insert-sort');
 const chai = require('chai');
 
 const expect = chai.expect;
-describe.only('Insert Sort Function', () => {
+describe('Insert Sort Function', () => {
   it('should use insert sort to sort the array', () => {
     const inputArray = [3, 2, 1, 5];
 
